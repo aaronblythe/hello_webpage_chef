8453732250bb8f2c386c1cafb6abf93d5b600496
jenkins <jenkins@hearst.at>
2016-04-24 15:07:33 +0000

    Bumping version to

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'andybly1@gmail.com'
 license 'all_rights'
 description 'Installs/Configures hello_webpage_chef'
 long_description 'Installs/Configures hello_webpage_chef'
-version '0.2.0'
+version '0.3.0'
 
 depends 'apache2'
 depends 'apt'
